1d7681597ac414949b5f891ca7527da31bc46cc8
Andre Williams <95472607+6Botter7@users.noreply.github.com>
2025-08-19 08:10:36 +0200

    styling changes

--- mui-practice/src/views/TodoList/ToDoListTaskContainer.tsx
+++ mui-practice/src/views/TodoList/ToDoListTaskContainer.tsx
@@ -4,9 +4,9 @@ import Button from '@mui/material/Button';
 
 function ToDoListTaskContainer() {
   return (
-    <List dense subheader={<li>Tasks To Do</li>} sx={{ bgcolor: 'background.paper' }}>
-      <ListItem divider secondaryAction={<Button>Action</Button>} sx={{ bgcolor: 'black' }}>Task 1</ListItem>
-      <ListItem divider secondaryAction={<Button>Action</Button>} sx={{ bgcolor: 'black' }}>Task 2</ListItem>
+    <List subheader={<li>Tasks To Do</li>} sx={{ bgcolor: '#282c34' , padding: 2 , marginTop: 2 }}>
+      <ListItem divider secondaryAction={<Button>Action</Button>} sx={{  }}>Task 1</ListItem>
+      <ListItem divider secondaryAction={<Button>Action</Button>} sx={{ }}>Task 2</ListItem>
     </List>
   );
 }
